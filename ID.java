631661cf8525ce997141a6bc063e0d501a22ba54
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-08-30 23:39:28 -0400

    I made preliminary methods for the ID class, which will guide me in the code I write in the main program

---
+++ ID.java
@@ -0,0 +1,41 @@
+public class ID{
+     private String name;
+     private String birthday;
+     private String year;//Class rank has been named 'year', as opposed to 'class' to avoid confusion with the code
+     private int overdue_cost;
+
+     public boolean taken;
+     
+
+
+     public ID(String a, String b, String c){
+         name = a;
+	 birthday = b;
+	 year = c;
+	 taken = false;
+	 overdue_cost = 0;
+     
+     }
+
+     public void displayID(){
+         System.out.println(name);
+	 System.out.println(birthday);
+	 System.out.println(year);
+     }
+
+     private int calculateFee(int minutes){
+      /*Flat rate fee of $1.50 will be applied for each minute gone over */
+
+	int fee = 1.5 * minutes;
+	overdue_cost = fee;
+	return fee;
+     }
+
+     public void userAction(){
+	 //This will let a user know if said person is on a machine    
+         taken = true;
+	 
+     }
+
+
+}
