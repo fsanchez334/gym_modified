a371f11a5d31c4d5c125a1f65e920f73ce04669f
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-08-30 23:09:11 -0400

    Modified main file to proper format

--- main.java
+++ main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public static void main(String[] args){
-     System.out.println("Hello and welcome to the gym!");
+public class Main{
 
+	public static void main(String[] args){
+	    System.out.println("Hello and welcome to the gym");
+	}
+	
 }
