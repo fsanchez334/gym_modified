980050206a8b7c48db0fae439442e93f69be9af7
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-08-30 23:16:35 -0400

    Testing to see if program takes in user information to make valid ID

--- main.java
+++ main.java
@@ -5,6 +5,14 @@ public class Main{
 
 	public static void main(String[] args){
 	    System.out.println("Hello and welcome to the gym");
+
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Please input your name: ");
+
+	    String name = input.nextLine();
+	    System.out.println(name);
+
+
 	}
 	
 }
