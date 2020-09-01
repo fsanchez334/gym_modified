d6cfdf343cffe24c95ccff8dee7682caf28ef333
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-09-01 18:04:13 -0400

    Made a new method that will indicate the person that has control of the treadmill

--- treadmill.java
+++ treadmill.java
@@ -4,10 +4,28 @@ public class Treadmill{
 
     public boolean taken;
     private String mode;
+    private String availability;
+
+    public ID person;
 
     public Treadmill(){
 	taken = false;
 	mode = "Default";
+	availability = "Available";
+	person = new ID();
+    }
+
+    public void displayInfo(){
+         System.out.println("-------TreadMill Information---------");
+	 System.out.println(mode);
+	 System.out.println(availability);
+    }
+
+    public String changeAndTake(String name){
+         taken = "true";
+	 String status = name + " has taken the treadmill";
+	 return status;
+
     }
 
 
