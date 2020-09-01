04fd167f6c8e964fcefbb6cdd3c77825596ba553
Fernando Sanchez <fernandosanchez955@gmail.com>
2020-09-01 00:58:20 -0400

    Made the constructor and the variable for the file

--- treadmill.java
+++ treadmill.java
@@ -1,3 +1,17 @@
 import java.util.*;
 
+public class Treadmill{
+    private ID holder;
+    public boolean taken;
+    private String mode;
+
+    public Treadmill(ID given){
+     
+        holder = given;
+	taken = false;
+	mode = "Default";
+    }
+
+
+}
 
